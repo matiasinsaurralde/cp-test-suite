dfe33adf0ec068d3bfa282575a34a925517bfd73
Matias Insaurralde <matias@insaurral.de>
2016-10-25 14:22:34 -0300

    Push test Go gRPC server.

---
+++ grpc/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"github.com/TykTechnologies/tyk/coprocess"
+)
+
+const (
+	port = ":5555"
+)
+
+type server struct{}
+
+func (s *server) Dispatch(ctx context.Context, object *coprocess.Object) (*coprocess.Object, error) {
+  log.Println("Dispatch")
+  object.Request.SetHeaders = make(map[string]string)
+  object.Request.SetHeaders["grpcheader"] = "grpcvalue"
+  return object, nil
+}
+
+func (s *server) DispatchEvent(ctx context.Context, object *coprocess.Event) (*coprocess.EventReply, error) {
+  return &coprocess.EventReply{}, nil
+}
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	coprocess.RegisterDispatcherServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
